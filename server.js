d28da84a4e93f6611cc53ea0f18e23379fc92aa6
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-12-08 18:00:49 +0100

    another one

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express();
 const pug = require("pug");
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
+const PORT = process.env.PORT || 5000;
 
 app.get("/", function (req, res) {
   const data = require("./graffiti.json");
   res.render("index", { data: data });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running at port 3000");
 });
