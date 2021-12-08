95c1a7966936e770e9d6ce789d6721698768b4dc
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-12-08 17:49:14 +0100

    this should work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const pug = require("pug");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 
